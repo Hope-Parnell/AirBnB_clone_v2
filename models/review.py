848fbc393227154ab44290fa8c64d5db335e66b2
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-10 02:44:00 -0600

    added user getter for filestorage

--- models/review.py
+++ models/review.py
@@ -2,6 +2,8 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from models.user import User
+import os
 
 
 class Review(BaseModel, Base):
@@ -12,3 +14,10 @@ class Review(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'),
                      nullable=False)
     text = Column(String(1024), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def user(self):
+            """gets the user who made the review"""
+            from models import storage
+            return storage.all(User).get("User.{}".format(self.user_id))
