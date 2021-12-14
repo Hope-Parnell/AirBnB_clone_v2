ab1e59958ebcbba1a2312f0cc3e19662bc520b30
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-14 11:55:41 -0600

    undid stuff

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 """Starts a Flask app"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+from models import state
 
-@app.route('/states')
-def states():
-    """
-    sets the route for '/states
-    """
-    return render_template('9-states.html',
-                            states=storage.all(State))
-
-@app.route('/states/<id>')
-def state(id):
-    """sets the route for '/states/<id>"""
-    return render_template('9-states.html',
-                            state=storage.all(State).get(
-                                "State.{}".format(id)))
-
-@app.teardown_appcontext
-def teardown(context):
-    """resets storage after each request"""
-    storage.close()
 
 if __name__ == '__main__':
+    from flask import Flask, render_template
+    from models import storage
+    from models.state import State
+
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+
+    @app.route('/states')
+    def states():
+        """
+        sets the route for '/states
+        """
+        return render_template('9-states.html',
+                               states=storage.all(State))
+
+    @app.route('/states/<id>')
+    def state(id=None):
+        """sets the route for '/states/<id>"""
+        return render_template('9-states.html',
+                               state=storage.all(State).get(
+                                   "State.{}".format(id)))
+
+    @app.teardown_appcontext
+    def teardown(context):
+        """resets storage after each request"""
+        storage.close()
+
     app.debug = True
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
