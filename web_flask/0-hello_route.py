438b4b3d77df993fb639d10cd4c5a8c2d456c69f
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-19 17:57:09 -0600

    changed only the run to main

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """Starts a Flask app"""
+from flask import Flask
 
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
+@app.route('/')
+@app.route('/airbnb-onepage/')
+def hello():
+    """
+    sets the route for '/'
+    """
+    return "Hello HBNB!"
 if __name__ == '__main__':
-    from flask import Flask
-
-    app = Flask(__name__)
-    app.url_map.strict_slashes = False
-
-    @app.route('/')
-    @app.route('/airbnb-onepage/')
-    def hello():
-        """
-        sets the route for '/'
-        """
-        return "Hello HBNB!"
-
     app.run(host='0.0.0.0', port='5000')
